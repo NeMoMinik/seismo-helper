c26e8a92d061cd9cf8434485bae1ca6e58fe725e
emil817 <eldamar17@mail.ru>
2023-07-04 17:49:50 +0700

    base table

--- seismo_helper/backend/views.py
+++ seismo_helper/backend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+# from .models import 
 # Create your views here.
+
+def get_table(request):
+    template = 'datatable/Datatable.html'
+    return render(request, template)
