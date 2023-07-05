fc4a664fce08e112028d17206a978dc1d2f91f44
NeMoMinikSAD <arseniyzigler99@gmail.com>
2023-07-05 15:49:13 +0700

    multiple traces

--- seismo_helper/data_table/dash/dash_chart.py
+++ seismo_helper/data_table/dash/dash_chart.py
@@ -3,6 +3,8 @@ import plotly.express as px
 from backend.models import Trace
 from django_plotly_dash import DjangoDash
 import scipy.io as sio
+import pandas
+import plotly.graph_objects as go
 
 app = DjangoDash('Chart')
 
@@ -23,6 +25,17 @@ def update_line_chart(value):
     for i in data:
         d = sio.loadmat(i.path)
         d = d['seismology'][0][0][-1].T
-        d = d[5]
-        fig = px.line(d)
+
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=[i for i in range(len(d[5]))], y=d[5],
+                                 mode='lines',
+                                 name='S'))
+        fig.add_trace(go.Scatter(x=[i for i in range(len(d[6]))], y=d[6],
+                                 mode='lines',
+                                 name='N'))
+        fig.add_trace(go.Scatter(x=[i for i in range(len(d[7]))], y=d[7],
+                                 mode='lines',
+                                 name='E'))
+        # fig.add_trace(go.Scatter(d[6], mode="lines", name='N'))
+        fig.update_layout(yaxis_title=i.station.name, xaxis_title='time', title="magnitude trace")
     return fig
