00f1f8812c5c4c3b8634f81e3e396021c89d1a9d
emil817 <eldamar17@mail.ru>
2023-07-06 14:10:30 +0700

    Update URLS

--- seismo_helper/backend/urls.py
+++ seismo_helper/backend/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import get_table, get_chart
 urlpatterns = [
-    path('DataTable/', get_table),
-    path('DataTable/<int:id_event>', get_chart)
+    path('Events/', get_table),
+    path('Events/<int:id_event>', get_chart)
 ]
