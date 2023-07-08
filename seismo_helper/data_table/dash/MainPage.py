43de98c0cc7241f6f2cd723d058c3c2394791acc
emil817 <eldamar17@mail.ru>
2023-07-08 20:26:41 +0700

    Files Upload

--- seismo_helper/data_table/dash/MainPage.py
+++ seismo_helper/data_table/dash/MainPage.py
@@ -9,8 +9,16 @@ from backend.models import Event, Station
 import plotly.express as px
 import dash_mantine_components as dmc
 from data_table.dash.Pageblank import footer, navbar
+import base64
+import os
+from urllib.parse import quote as urlquote
+from flask import Flask 
+import io
 
-app = DjangoDash('DashDatatable',external_stylesheets=[dbc.themes.BOOTSTRAP])
+external_stylesheets_downl = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+UPLOAD_DIRECTORY = os.getcwd() + "\\media\\"
+
+app = DjangoDash('DashDatatable',external_stylesheets=[dbc.themes.BOOTSTRAP, external_stylesheets_downl])
 
 BASE_LINK = 'http://127.0.0.1:8000/Events/'
 vv = Event.objects.all().values('id', 'location__name', 'time', 'x', 'y', 'z', 'magnitude')
@@ -116,7 +124,26 @@ table_css = [
 ]
 app.layout = html.Div([
     navbar,
-    dcc.Dropdown(['Все']+ list(set([x for x in mdf['Локация']])), 'Все', id='loc-dropdown'),
+    html.Div(dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Miniseed Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '40px',
+            'lineHeight': '40px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px',
+            'vertical-align': 'middle'
+        },
+        multiple=False
+    ),style={'width': '49%', 'display': 'inline-block'}),
+    html.Div(dcc.Dropdown(['Все']+ list(set([x for x in mdf['Локация']])), 'Все', id='loc-dropdown'),style={'width': '45%', 'display': 'inline-block', 'float':'right', 'textAlign': 'center', 'margin': '10px','height': '40px'}),
     dcc.Graph(figure=fig, id='mapD'),
     dash_table.DataTable(
         id='datatable-interactivity',
@@ -127,8 +154,22 @@ app.layout = html.Div([
         sort_mode="single",
         style_cell={'textAlign': 'center'},
     ),
-    footer,
+    dcc.Store(id='store'),
+    footer
 ])
+
+
+@app.callback(Output('store', 'data'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(contents, list_of_names, list_of_dates):
+    if contents is None:
+        return None
+    with open(UPLOAD_DIRECTORY + list_of_names, "wb") as fh:
+        data = contents.encode("utf8").split(b";base64,")[1]
+        fh.write(base64.decodebytes(data))
+
 
 
 #UPDATE
--- seismo_helper/data_table/dash/Pageblank.py
+++ seismo_helper/data_table/dash/Pageblank.py
@@ -3,19 +3,11 @@ import dash_bootstrap_components as dbc
 from dash import html, dcc, no_update, Dash, dash_table, callback
 BASE_LINK = 'http://127.0.0.1:8000/Events/'
 
+#dbc.DropdownMenu(children=[dbc.DropdownMenuItem("More pages", header=True),dbc.DropdownMenuItem("Page 2", href="#"),dbc.DropdownMenuItem("Page 3", href="#"),],nav=True,in_navbar=True,label="More",),
 navbar = dbc.NavbarSimple(
     children=[
         dbc.NavItem(dbc.NavLink("Main Page", href=BASE_LINK, target='_blank')),
-        dbc.DropdownMenu(
-            children=[
-                dbc.DropdownMenuItem("More pages", header=True),
-                dbc.DropdownMenuItem("Page 2", href="#"),
-                dbc.DropdownMenuItem("Page 3", href="#"),
-            ],
-            nav=True,
-            in_navbar=True,
-            label="More",
-        ),
+        dbc.NavItem(dbc.NavLink("About", href=BASE_LINK, target='_blank')),
     ],
     brand="Прикол",
     brand_href="#",
@@ -30,5 +22,5 @@ footer = dmc.Footer(
     dmc.Text("Контакты:"),
     html.Div(html.A("Телеграмм", href='https://t.me/Emil817', target="_blank"))
     ],
-    style={"backgroundColor": "#9c86e2"},
+    style={"backgroundColor": "#0D6EFD"},
 )
