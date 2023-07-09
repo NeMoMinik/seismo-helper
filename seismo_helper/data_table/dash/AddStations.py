e784b99a5b432ee8a7fd4b2c7bf6dfa99526b478
emil817 <eldamar17@mail.ru>
2023-07-09 21:16:01 +0700

    StationsDatatable

--- seismo_helper/data_table/dash/AddStations.py
+++ seismo_helper/data_table/dash/AddStations.py
@@ -13,17 +13,43 @@ import requests as rq
 DATABASE_API = f'http://{ALLOWED_HOSTS[0]}:8000/api/'
 BASE_LINK = f'http://{ALLOWED_HOSTS[0]}:8000/Events/'
 app = DjangoDash('AddStations',external_stylesheets=[dbc.themes.BOOTSTRAP])
-
+fupd = 0
+table_columns = [
+    {
+        'id': '0',
+        'name': '№',
+        'sortable': True,
+        'textAlign': 'center'
+    },
+    {
+        'id': '1',
+        'name': 'name',
+        'sortable': True,
+        'textAlign': 'center'
+    },
+    {
+        'id': '2',
+        'name': 'X',
+        'sortable': True,
+        'textAlign': 'center'
+    },
+    {
+        'id': '3',
+        'name': 'Y',
+        'sortable': True,
+        'textAlign': 'center'
+    },
+    {
+        'id': '4',
+        'name': 'Z',
+        'sortable': True,
+        'textAlign': 'center'
+    }]
 
 app.layout = html.Div([
     navbar,
     html.H1('Добавление станций приёма сигнала'),
     html.Div(id='ddd', children=[dcc.Dropdown(['Локация'], 'Локация', id='dd')]),
-    dcc.Input(id='name', placeholder='Название', type='text'),
-    dcc.Input(id='X', placeholder='Широта', type='float'),
-    dcc.Input(id='Y', placeholder='Долгота', type='float'),
-    dcc.Input(id='Z', placeholder='Высота над уровнем моря', type='float'),
-    html.Button('Добавить', id='submit-val', n_clicks=0),
     html.Div(id='container-button-basic'),
     footer
 ])
@@ -33,9 +59,33 @@ app.layout = html.Div([
     Input('dd', 'value')
 )
 def upd_dd(value):
+    global fupd
     vv = rq.get(DATABASE_API + 'locations/').json()['results']
+    dt = rq.get(DATABASE_API + 'stations/').json()['results']
+    S = [[],[],[],[], []]
+    for i in dt:
+        S[0].append(i['id'])
+        S[1].append(i['name'])
+        S[2].append(i['x'])
+        S[3].append(i['y'])
+        S[4].append(i['z'])
     A = [{'label': x['name'], 'value':x['id']} for x in vv]
-    return [dcc.Dropdown(options=A, value=value, id='dd')]
+    df = pd.DataFrame(S).T.sort_values(0)
+    if fupd == 0:
+        fupd += 1
+        return [dcc.Dropdown(options=A, value=value, id='dd'),
+                dcc.Input(id='name', placeholder='Название', type='text'),
+                dcc.Input(id='X', placeholder='Широта', type='float'),
+                dcc.Input(id='Y', placeholder='Долгота', type='float'),
+                dcc.Input(id='Z', placeholder='Высота над уровнем моря', type='float'),
+                html.Button('Добавить', id='submit-val', n_clicks=0),
+                dash_table.DataTable(
+                    id='datatable',
+                    columns=table_columns,
+                    data=df.to_dict('records'),style_cell={'textAlign': 'center'})]
+    else:
+        return no_update
+
 
 @app.callback(
     Output('container-button-basic', 'children'),
@@ -47,7 +97,7 @@ def upd_dd(value):
     State('dd', 'value')
 )
 def update_output(n_clicks, x, y, z, name, loc_id):
-    if loc_id != 'Локация':
+    if x != None and loc_id != 'Локация':
         data = {
             "name": name,
             "x":x,
