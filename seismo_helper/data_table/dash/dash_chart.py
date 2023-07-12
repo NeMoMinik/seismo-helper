bb36a06158f30c0a387db691283e1cee5a480d10
NeMoMinikSAD <arseniyzigler99@gmail.com>
2023-07-12 13:54:58 +0300

    trace size fix

--- seismo_helper/data_table/dash/dash_chart.py
+++ seismo_helper/data_table/dash/dash_chart.py
@@ -19,7 +19,7 @@ app.layout = html.Div([
     footer
     ]
 )
-DATABASE_API =  f'http://{ALLOWED_HOSTS[0]}:8000/api/'
+DATABASE_API = f'http://{ALLOWED_HOSTS[0]}:8000/api/'
 
 
 @app.callback(
@@ -42,4 +42,5 @@ def update_line_chart(value):
                           row=n + 1
                           )
             fig.update_yaxes(title_text=i['station'], col=1, row=n + 1)
+    fig.update_layout(height=500 * len(data))
     return fig
